4bbfcdb6972dbd52935da00dd4ad2254d9304a0f
Deeved Hiuston <deeved@gmail.com>
2021-08-30 22:43:28 -0300

    [letmeask-aula 04] interação com salas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -33,11 +33,15 @@ export function Home() {
     if(roomCode.trim() === ''){
       return
     }
-
     const roomRef = await database.ref(`/rooms/${roomCode}`).get()
 
     if(!roomRef.exists()){
       alert('Room does not exists.')
+      return
+    }
+
+    if(roomRef.val().endedAt) {
+      alert('Room already closed.')
       return
     }
 
